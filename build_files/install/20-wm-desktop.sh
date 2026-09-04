efc7c442136801fbce95d705087c2fd17eb86e9f
Friesi <friesi@srvb.at>
2026-09-04 12:43:13 +0000

    Add Hyprland and DankLinux repositories and install packages in 20-wm-desktop.sh

--- build_files/install/20-wm-desktop.sh
+++ build_files/install/20-wm-desktop.sh
@@ -4,9 +4,25 @@ set -euo pipefail
 
 FEDORA_VERSION="$(rpm -E '%{fedora}')"
 
+dnf5 config-manager addrepo --from-repofile="https://copr.fedorainfracloud.org/coprs/lionheartp/Hyprland/repo/fedora-${FEDORA_VERSION}/lionheartp-Hyprland-fedora-${FEDORA_VERSION}.repo"
+dnf5 config-manager setopt copr:copr.fedorainfracloud.org:lionheartp:Hyprland.enabled=0
+
+dnf5 config-manager addrepo --from-repofile="https://copr.fedorainfracloud.org/coprs/avengemedia/danklinux/repo/fedora-${FEDORA_VERSION}/avengemedia-danklinux-fedora-${FEDORA_VERSION}.repo"
+dnf5 config-manager setopt copr:copr.fedorainfracloud.org:avengemedia:danklinux.enabled=0
+
 dnf5 config-manager addrepo --from-repofile="https://copr.fedorainfracloud.org/coprs/avengemedia/dms-git/repo/fedora-${FEDORA_VERSION}/avengemedia-dms-git-fedora-${FEDORA_VERSION}.repo"
 dnf5 config-manager setopt copr:copr.fedorainfracloud.org:avengemedia:dms-git.enabled=0
 
 dnf5 install -y \
+    --enablerepo=copr:copr.fedorainfracloud.org:lionheartp:Hyprland \
+    --enablerepo=copr:copr.fedorainfracloud.org:avengemedia:danklinux \
 	--enablerepo=copr:copr.fedorainfracloud.org:avengemedia:dms-git \
-    dms
+    hyprland \
+    hyprland-guiutils \
+    dms \
+    dms-greeter
+
+
+dms-greeter install
+
+
