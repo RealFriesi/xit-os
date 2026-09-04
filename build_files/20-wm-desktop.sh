dc14e94e78de3bbc02d7ee871be9c39ecaa01e88
Friesi <friesi@srvb.at>
2026-09-04 12:24:45 +0000

    Add script to install dms-git from COPR repository for Fedora

---
+++ build_files/20-wm-desktop.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -euo pipefail
+
+FEDORA_VERSION="$(rpm -E '%{fedora}')"
+
+dnf5 config-manager addrepo --from-repofile="https://copr.fedorainfracloud.org/coprs/avengemedia/dms-git/repo/fedora-${FEDORA_VERSION}/avengemedia-dms-git-fedora-${FEDORA_VERSION}.repo"
+dnf5 config-manager setopt copr:copr.fedorainfracloud.org:avengemedia:dms-git.enabled=0
+
+dnf5 install -y \
+	--enablerepo=copr:copr.fedorainfracloud.org:avengemedia:dms-git \
+    dms-git
