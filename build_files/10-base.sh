0d2fb0f310474299fe79a420a17c52388e50e36b
Friesi <friesi@srvb.at>
2026-09-04 12:15:24 +0000

    Refactor package installation in build.sh and add 10-base.sh for essential packages

---
+++ build_files/10-base.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -euo pipefail
+
+dnf5 install -y \
+	dnf5-plugins \
+	distrobox \
+	fish \
+	flatpak \
+	fuse \
+	fuse-libs \
+	bluez \
+	btrfs-progs \
+	cryptsetup \
+	fwupd \
+	glibc-all-langpacks \
+	langpacks-de \
+	langpacks-en \
+	NetworkManager \
+	pipewire \
+	pipewire-pulseaudio \
+	polkit \
+	power-profiles-daemon \
+	udisks2 \
+	wireplumber \
+	xdg-user-dirs
