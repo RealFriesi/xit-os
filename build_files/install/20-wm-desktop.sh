4823e9e4b243e415c3b5010eb136f466178293f6
Friesi <friesi@srvb.at>
2026-09-07 09:30:17 +0000

    Refactor wm-desktop installation script: replace multiple COPR repositories with a single Terra repository and update package installation list

--- build_files/install/20-wm-desktop.sh
+++ build_files/install/20-wm-desktop.sh
@@ -4,25 +4,15 @@ set -euo pipefail
 
 FEDORA_VERSION="$(rpm -E '%{fedora}')"
 
-dnf5 config-manager addrepo --from-repofile="https://copr.fedorainfracloud.org/coprs/lionheartp/Hyprland/repo/fedora-${FEDORA_VERSION}/lionheartp-Hyprland-fedora-${FEDORA_VERSION}.repo"
-dnf5 config-manager setopt copr:copr.fedorainfracloud.org:lionheartp:Hyprland.enabled=0
-
-dnf5 config-manager addrepo --from-repofile="https://copr.fedorainfracloud.org/coprs/avengemedia/danklinux/repo/fedora-${FEDORA_VERSION}/avengemedia-danklinux-fedora-${FEDORA_VERSION}.repo"
-dnf5 config-manager setopt copr:copr.fedorainfracloud.org:avengemedia:danklinux.enabled=0
-
-dnf5 config-manager addrepo --from-repofile="https://copr.fedorainfracloud.org/coprs/avengemedia/dms-git/repo/fedora-${FEDORA_VERSION}/avengemedia-dms-git-fedora-${FEDORA_VERSION}.repo"
-dnf5 config-manager setopt copr:copr.fedorainfracloud.org:avengemedia:dms-git.enabled=0
+dnf5 config-manager addrepo --from-repofile="https://raw.githubusercontent.com/terrapkg/subatomic-repos/main/terra.repo"
+dnf5 config-manager setopt terra.enabled=0
 
 dnf5 install -y \
-    --enablerepo=copr:copr.fedorainfracloud.org:lionheartp:Hyprland \
-    --enablerepo=copr:copr.fedorainfracloud.org:avengemedia:danklinux \
-	--enablerepo=copr:copr.fedorainfracloud.org:avengemedia:dms-git \
-    --exclude=wofi \
-	--exclude=nwg-panel \
-    hyprland \
-    hyprland-guiutils \
-    dms \
-    dms-greeter \
+    --enablerepo=terra \
+    xwayland-satellite \
+    noctalia \
+    noctalia-greeter \
+    umbriel-nightly \
     adw-gtk3-theme \
 	hyprqt6engine \
     gnome-keyring \
@@ -31,6 +21,4 @@ dnf5 install -y \
 
 
 systemctl enable greetd.service
-install -d /etc/systemd/user/hyprland-session.target.wants
-ln -sf /usr/lib/systemd/user/dms.service /etc/systemd/user/hyprland-session.target.wants/dms.service
 systemctl set-default graphical.target
