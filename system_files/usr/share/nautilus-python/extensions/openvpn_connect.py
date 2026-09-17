0e88878cd2cfaef7df0e23b9a8a022c8660a8089
Friesi <friesi@srvb.at>
2026-09-17 19:48:40 +0000

    Refactor OpenVPN connection handling: separate local and remote config copying, update Nautilus extension to use new config location function

--- system_files/usr/share/nautilus-python/extensions/openvpn_connect.py
+++ system_files/usr/share/nautilus-python/extensions/openvpn_connect.py
@@ -6,24 +6,19 @@ from urllib.parse import unquote, urlparse
 
 import gi
 
-try:
-    gi.require_version("Nautilus", "4.0")
-except ValueError:
-    gi.require_version("Nautilus", "3.0")
+gi.require_version("Nautilus", "4.0")
 
 from gi.repository import GObject, Nautilus  # noqa: E402
 
 HELPER = "/usr/libexec/xit-openvpn-connect"
 
 
-def _local_path(file_info):
+def _config_location(file_info):
     uri = file_info.get_uri()
     scheme = urlparse(uri).scheme
     if scheme == "file":
         return unquote(urlparse(uri).path)
-    # Samba/GVFS-Mounts sind über den FUSE-Pfad lesbar.
-    mapped = file_info.get_location().get_path()
-    return mapped
+    return uri
 
 
 class OpenVPNConnectExtension(GObject.GObject, Nautilus.MenuProvider):
@@ -39,8 +34,8 @@ class OpenVPNConnectExtension(GObject.GObject, Nautilus.MenuProvider):
         if not os.path.isfile(HELPER):
             return []
 
-        path = _local_path(file_info)
-        if not path:
+        location = _config_location(file_info)
+        if not location:
             return []
 
         item = Nautilus.MenuItem(
@@ -48,22 +43,21 @@ class OpenVPNConnectExtension(GObject.GObject, Nautilus.MenuProvider):
             label="Verbinden (OpenVPN)",
             tip="Konfiguration in einen temporären Ordner kopieren und verbinden",
         )
-        item.connect("activate", self._on_activate, path)
+        item.connect("activate", self._on_activate, location)
         return [item]
 
-    # Nautilus 4.0: (files); Nautilus 3.0: (window, files)
-    def get_file_items(self, *args):
-        return self._menu_items(args[-1])
+    def get_file_items(self, files):
+        return self._menu_items(files)
 
-    def _on_activate(self, _menu, path):
+    def _on_activate(self, _menu, location):
         subprocess.Popen(
             [
                 "ghostty",
                 "--title",
-                "OpenVPN: {}".format(os.path.basename(path)),
+                "OpenVPN: {}".format(os.path.basename(location)),
                 "-e",
                 HELPER,
-                path,
+                location,
             ],
             start_new_session=True,
         )
