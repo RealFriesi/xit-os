cdce319b19cc63745835e7d64109f59a1b240c88
Friesi <friesi@srvb.at>
2026-09-17 11:14:03 +0000

    Remove flatpak preinstall command from setup script

--- build_files/install/10-setup-repos.sh
+++ build_files/install/10-setup-repos.sh
@@ -21,4 +21,3 @@ gpgkey=https://keys.anydesk.com/repos/RPM-GPG-KEY
 EOF
 
 flatpak remote-add --if-not-exists --system flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak preinstall --system --noninteractive
